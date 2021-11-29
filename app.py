dfeefeaae05445c8c6faf229814e9811577f78c5
rafikee <rafik.vcu@gmail.comm>
2021-11-29 10:05:20 -0500

    added delete function

--- app.py
+++ app.py
@@ -7,20 +7,41 @@ app = Flask(__name__)
 
 # show the form to submit the upload
 @app.route('/upload/<athan>', methods = ['GET', 'POST'])
-def choose_file(athan, file_path = ""):
+def choose_file(athan):
    confirmation = ""
    if request.method == 'POST':
-      f = request.files['file']
-      filename = secure_filename(f.filename)
-      f.save(os.path.join('static/' + athan, filename))
-      confirmation = "Successfully uploaded " + filename
-   if athan == 'delete':
-      athan = 'deleted'
-      file_name = file_path.split('/')[1]
-      os.rename(f'static/{file_path}', f"static/deleted/{file_name}")
-
-   filenames = next(walk('static/' + athan), (None, None, []))[2]  # [] if no file
-   audio_files = [athan +'/' + x for x in filenames]
+      if athan == 'delete_forever':
+         file_path = request.form['file_path']
+         athan = 'deleted'
+         file_name = file_path.split('/')[1]
+         os.remove(f'static/{file_path}')
+         confirmation = "permanently removed " + file_path
+      elif athan == 'restore':
+         file_path = request.form['file_path']
+         athan = file_path.split('/')[1]
+         file_name = file_path.split('/')[1] + '/' + file_path.split('/')[2]
+         os.rename(f'static/{file_path}', f"static/{file_name}")
+         confirmation = "restored " + file_name
+      elif athan == 'delete':
+         file_path = request.form['file_path']
+         athan = 'deleted'
+         os.rename(f'static/{file_path}', f"static/deleted/{file_path}")
+         confirmation = "deleted " + file_path
+      else:
+         f = request.files['file']
+         filename = secure_filename(f.filename)
+         f.save(os.path.join('static/' + athan, filename))
+         confirmation = "Successfully uploaded " + filename
+   
+   # Display the files in the folder
+   if athan != 'deleted':
+      filenames = next(walk('static/' + athan), (None, None, []))[2]  # [] if no file
+      audio_files = [athan +'/' + x for x in filenames]
+   else:
+      audio_files = []
+      for folder in next(os.walk('static/deleted'))[1]:
+         filenames = next(walk('static/' + athan + '/' + folder), (None, None, []))[2]  # [] if no file
+         audio_files.extend([athan +'/' + folder + '/' + x for x in filenames])
    return render_template('upload.html', audio_files=audio_files, athan=athan, confirmation=confirmation)
 		
 if __name__ == '__main__':
